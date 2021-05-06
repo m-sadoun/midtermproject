2d62a37312170084459935959487f48125808b6e
m-sadoun <sadoumenad@gmail.com>
2021-05-05 21:59:16 -0600

    initial commit

---
+++ src/design/AbstructClass.java
@@ -0,0 +1,12 @@
+package design;
+
+public class AbstructClass {
+    public void sickTime(){
+        System.out.println("employees should get paid for sicktime");
+
+
+
+
+
+    }
+}
---
+++ src/math/problems/Pattern.java
@@ -0,0 +1,43 @@
+package math.problems;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        /* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
+         * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
+         *
+         *
+         */
+        int count = 0;
+        int i;
+        for (i = 100; i >= 0; i--) {
+            count++;
+            if (count <= 10) {
+                System.out.print(i+",");
+            } else {
+                for (i = 90; i >= 0; i -= 2) {
+                    count++;
+                    if (count <= 22) {
+                        System.out.print(i+",");
+                    } else {
+                        for (i = 67; i >= 0; i -= 3) {
+                            count++;
+                            if (count <= 32) {
+                                System.out.print(i+",");
+                            } else {
+                                for (i = 40; i >= 0; i -= 4) {
+                                    count++;
+                                    if (count <= 44) {
+                                        System.out.print(i+",");
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
